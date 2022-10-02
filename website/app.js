1c92d6b28549bb9e24ab88101973bb5d498a75d2
mariam-elmadawy <91071088+mariam-elmadawy@users.noreply.github.com>
2022-10-02 15:28:26 +0200

    update app.js and readme

--- website/app.js
+++ website/app.js
@@ -1,43 +1,43 @@
-/* Global Variables */
-// Personal API Key for OpenWeatherMap API
+// base url of OpenWeather api
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiK = "&appid=83b008e28e4f9282388726aee5532a77&units=imperial";
+// my own key from api
+const apiPersonalK = "&appid=83b008e28e4f9282388726aee5532a77&units=imperial";
+// intial server for post and get
 const server = "http://127.0.0.1:4000";
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
-
-// Event listener to add function to existing HTML DOM element
+// global variables
+let dDate = new Date();
+let newDate =
+    dDate.getMonth() + "." + dDate.getDate() + "." + dDate.getFullYear();
 const generate = document.querySelector("#generate");
-generate.addEventListener("click", getInfo);
 
-/* Function called by event listener */
-function getInfo(e) {
-    const zip = document.getElementById("zip").value;
+let generateInfo = function() {
+    const zipCode = document.getElementById("zip").value;
     const feeling = document.getElementById("feelings").value;
-    getWeatherInfo(baseUrl, zip, apiK).then(function(data) {
+    getWInfo(baseUrl, zipCode, apiPersonalK).then(function(data) {
         console.log(data);
-        postData(server + "/add", {
-            date: d,
+        postUserData(server + "/add", {
+            date: dDate,
             temp: data.main.temp,
             content: feeling,
+        }).then(function() {
+            UpdateWeatherUI();
         });
-        retrieveData();
     });
-}
-/* Function to GET Web API Data*/
-const getWeatherInfo = async(baseUrl, zip, key) => {
-    const res = await fetch(baseUrl + zip + key);
-    try {
-        const data = await res.json();
+};
+//event listener to generate button
+generate.addEventListener("click", generateInfo);
 
+const getWInfo = async(bUrl, z, k) => {
+    const response = await fetch(bUrl + z + k);
+    try {
+        const data = await response.json();
         return data;
-    } catch (error) {
-        console.log("error", error);
+    } catch (e) {
+        console.log("error", e);
     }
 };
-/* Function to POST data */
-const postData = async(url = "", data = {}) => {
+//Function POST data
+const postUserData = async(url = "", data = {}) => {
     console.log(data);
     const response = await fetch(url, {
         method: "POST",
@@ -45,7 +45,6 @@ const postData = async(url = "", data = {}) => {
         headers: {
             "Content-Type": "application/json",
         },
-        // Body data type must match "Content-Type" header
         body: JSON.stringify({
             temp: data.temp,
             date: data.date,
@@ -56,24 +55,22 @@ const postData = async(url = "", data = {}) => {
         const newData = await response.json();
         console.log(newData);
         return newData;
-    } catch (error) {
-        console.log("error", error);
+    } catch (e) {
+        console.log("error", e);
     }
 };
 
-/* Function to GET Project Data */
-const retrieveData = async() => {
-    const req = await fetch(server + "/all");
+// Function that update ui
+const UpdateWeatherUI = async() => {
+    const request = await fetch(server + "/all");
     try {
-        // Transform into JSON
-        const allData = await req.json();
+        const allData = await request.json();
         console.log(allData);
-        // Write updated data to DOM elements
         document.getElementById("temp").innerHTML =
             Math.round(allData.temp) + "degrees";
         document.getElementById("content").innerHTML = allData.content;
         document.getElementById("date").innerHTML = allData.date;
-    } catch (error) {
-        console.log("error", error);
+    } catch (e) {
+        console.log("error", e);
     }
 };
